354a459c4e6bddae336b16e32189ec489b06a298
Thiago Chaves Santos <thiagocsfoz@gmail.com>
2021-04-22 16:35:12 -0300

    fix url api.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://cors-anywhere.herokuapp.com/https://ir-bolsa-api.herokuapp.com/api/v1',
+    baseURL: 'https://ir-bolsa-api.herokuapp.com/api/v1',
 });
 
 export default api;
